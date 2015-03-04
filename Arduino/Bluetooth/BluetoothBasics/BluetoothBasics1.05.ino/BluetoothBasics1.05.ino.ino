28a647c90c815f94773a2fa7a456f72002bbebef
UserK <balestrieri.gepp@gmail.com>
2015-03-04 04:11:18 +0100

    Added Bt 1.05

---
+++ Arduino/Bluetooth/BluetoothBasics/BluetoothBasics1.05.ino/BluetoothBasics1.05.ino.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>  
+
+int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
+int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+void setup()
+{
+  Serial.begin(9600);  // Begin the serial monitor at 9600bps
+
+  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+  bluetooth.print("$$$");  // Print three times individually
+  delay(100);  // Short delay, wait for the Mate to send back CMD
+  bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
+  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
+  bluetooth.begin(9600);  // Start bluetooth serial at 9600
+}
+// Dai $$$ -> set NL in terminal monitor -> I (inquiry) -> C,name -> 
+
+void loop()
+{
+  if(bluetooth.available())  // If the bluetooth sent any characters
+  {
+    // Send any characters the bluetooth prints to the serial monitor
+    Serial.print((char)bluetooth.read());  
+  }
+  if(Serial.available())  // If stuff was typed in the serial monitor
+  {
+    // Send any characters the Serial monitor prints to the bluetooth
+    bluetooth.print((char)Serial.read());
+  }
+  // and loop forever and ever!
+}
