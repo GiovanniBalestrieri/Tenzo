d984ea328cba2e62ee9eeab5e28b70f9f7fc7752
balestrieri.gepp@gmail.com <gepp88@balestrieri.gepp@gmail.com>
2015-03-12 15:55:29 +0100

    Added second encoder

--- Arduino/GyroTest/GyroAngle/GyroAngle.ino
+++ Arduino/GyroTest/GyroAngle/GyroAngle.ino
@@ -117,15 +117,17 @@ int setupL3G4200D(int scale)
   //From  Jim Lindblom of Sparkfun's code
 
   // Enable x, y, z and turn off power down:
-  writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
+  //writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
+  // 400Hz and 1Hz cutoff frq of the HPF
+  writeRegister(L3G4200D_Address, CTRL_REG1, 0b01011111);
 
   // If you'd like to adjust/use the HPF, you can edit the line below to configure CTRL_REG2:
-  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000000);
+  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000100);
 
   // Configure CTRL_REG3 to generate data ready interrupt on INT2
   // No interrupts used on INT1, if you'd like to configure INT1
   // or INT2 otherwise, consult the datasheet:
-  writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
+  //writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
 
   // CTRL_REG4 controls the full-scale range, among other things:
 
