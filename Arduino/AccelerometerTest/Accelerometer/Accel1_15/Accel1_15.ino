db80541bca6255508aa4816df560a40b462c59c9
balestrieri.gepp@gmail.com <gepp88@balestrieri.gepp@gmail.com>
2015-03-17 21:23:08 +0100

    Added Arduino-Processing Angle estimation from Acc & Triple plot

---
+++ Arduino/AccelerometerTest/Accelerometer/Accel1_15/Accel1_15.ino
@@ -0,0 +1,235 @@
+//#define DEBUGMODE
+
+const float RESOLUTION=800; //0.8 v/g -> resolucion de 1.5g -> 800mV/g
+const float VOLTAGE=3.3;  //voltage al que está conectado el acelerómetro
+
+const float ZOUT_1G = 850;   // mv Voltage en Zout a 1G
+
+const int NADJ  = 50;        // Número de lecturas para calcular el error
+
+// Entradas analógicas donde van los sensores
+const int xaxis = 0;
+const int yaxis = 1;
+const int zaxis = 2;
+
+float XError,YError,ZError;
+float xd,yd,zd,z,x,y;
+
+// Acc Timers
+unsigned long accTimer;
+unsigned long lastAccTimer;
+unsigned long timeToRead = 0;
+unsigned long lastTimeToRead = 0;
+
+
+// delta T control the routine frequency
+float deltaT = 5;
+float timerLoop = 0, timerReading = 0, timerSec = 0;
+float timerRoutine = 0, count = 0;
+float redingTime = 0, samplingTime = 0, calcTime =0, printTime = 0;
+float k=0, kM1=0, kMReading = 0, kMRoutine=0, kMLoop=0, secRoutine=0;
+
+byte mode;
+
+// Define various ADC prescaler
+const unsigned char PS_16 = (1 << ADPS2);
+const unsigned char PS_32 = (1 << ADPS2) | (1 << ADPS0);
+const unsigned char PS_64 = (1 << ADPS2) | (1 << ADPS1);
+const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+
+
+boolean processing = false;
+// Timer variables
+volatile int cont = 0;
+volatile int contSamples=0;
+volatile int contCalc=0;
+
+volatile float angleXAcc;
+volatile float angleYAcc;
+
+volatile float aax,aay,aaz;
+volatile float alphaA = 0.2, alphaW = 0.8;
+
+void setup()
+{  
+ Serial.begin(115200); // 9600 bps
+ pinMode(xaxis,INPUT);
+ pinMode(yaxis,INPUT);
+ pinMode(zaxis,INPUT);
+
+ XError =  AccelAdjust(xaxis);
+ YError =  AccelAdjust(yaxis);
+ ZError =  AccelAdjust(zaxis);
+ ZError = ZError - ZOUT_1G;
+ 
+ // Timer settings
+   // Initialize Timer
+  cli();
+  TCCR3A = 0;
+  TCCR3B = 0;
+
+  // Set compare match register to the desired timer count
+  //OCR3A=77; //16*10^6/(200Hz*1024)-1 = 77 -> 200 Hz 
+  OCR3A=193; //16*10^6/(80Hz*1024)-1 = 193 -> 80 Hz 
+  //OCR3A=780; //16*10^6/(20Hz*1024)-1 = 780 -> 20 Hz 
+  //OCR3A=50; //16*10^6/(308Hz*1024)-1 = 50 -> 308 Hz 
+
+  TCCR3B |= (1 << WGM32);
+  // Set CS10 and CS12 bits for 1024 prescaler:
+  TCCR3B |= (1 << CS30) | (1 << CS32);
+  // enable timer compare interrupt:
+  TIMSK3 |= (1 << OCIE3B);
+
+  // enable global interrupts:
+  sei(); 
+ 
+ // ADC Tuning
+ ADCSRA &= ~PS_128;  // remove bits set by Arduino library
+ // you can choose a prescaler from above.
+ // PS_16, PS_32, PS_64 or PS_128
+ ADCSRA |= PS_32;    // set our own prescaler to 64 
+ 
+}
+
+void loop()
+{
+ //accRoutine(); 
+ serialRoutine();
+ delay(20);
+} 
+
+ISR(TIMER3_COMPB_vect)
+{  
+  // Mettilo altrimenti non funziona
+  sei();
+  // Update x, y, and z with new values 100 ns     
+  //read values
+  //getGyroValues(); 
+  getAcc();
+  calcAngle();
+  cont++;
+}
+
+void getAcc() //ISR
+{
+   x=analogRead(xaxis);
+   y=analogRead(yaxis);
+   z=analogRead(zaxis);
+  
+   aax = (((x*5000.0)/1023.0)-XError)/RESOLUTION;
+   aay = (((y*5000.0)/1023.0)-YError)/RESOLUTION;
+   aaz = (((z*5000.0)/1023.0)-ZError)/RESOLUTION;
+  
+   // gets the value sample time
+   accTimer = micros() - lastAccTimer;
+   // updates last reading timer
+   lastAccTimer = micros();  
+}
+
+void calcAngle() //ISR
+{
+  /*
+   volatile float accs[3];        
+   accs[0] = (((aax*5000.0)/1023.0)-XError)/RESOLUTION;  
+   accs[1] = (((aay*5000.0)/1023.0)-YError)/RESOLUTION;
+   accs[2] = (((aaz*5000.0)/1023.0)-ZError)/RESOLUTION;
+   */
+   /*
+   accButter3(accs);
+   axF = accs[0];
+   ayF = accs[1];
+   azF = accs[2];
+   
+   */
+//  angleXAcc = (atan2(-accs[0],accs[2])) * RAD_TO_DEG;
+//  angleYAcc = (atan2(accs[1],accs[2])) * RAD_TO_DEG;
+  angleXAcc = (atan2(-aax,aaz)) * RAD_TO_DEG;
+  angleYAcc = (atan2(aay,aaz)) * RAD_TO_DEG;
+}
+
+void wFilter(volatile float val[])
+{
+  val[0] = (1-alphaW)*x + alphaW*val[0];
+  val[1] = (1-alphaW)*y + alphaW*val[1];
+  val[2] = (1-alphaW)*z + alphaW*val[2];
+}
+
+void accFilter(volatile float val[])
+{
+  val[0] = (1-alphaA)*aax + alphaW*val[0];
+  val[1] = (1-alphaA)*aay + alphaW*val[1];
+  val[2] = (1-alphaA)*aaz + alphaW*val[2];
+}
+
+void serialRoutine()
+{
+  
+  timerSec = micros()-secRoutine;
+   lastTimeToRead = micros();
+   
+   if (timerSec >= 1000000)
+  {
+    secRoutine = micros();
+    Serial.print(cont);
+    Serial.print("    Smpl: ");
+    Serial.print(contSamples);
+    Serial.print("    calc: ");
+    Serial.println(contCalc);
+    /*
+    Serial.print("    wX: ");
+    Serial.print(wF[0]);
+    Serial.print("    Wy: ");
+    Serial.println(wF[1]);
+    Serial.print("    Wz: ");
+    Serial.println(wF[2]);
+    */
+    cont=0;      
+    contSamples=0;      
+    contCalc=0;      
+  }
+
+  timerRoutine = micros()-kMRoutine;
+  if (timerRoutine >= deltaT*1000)
+  {      
+    kMRoutine = micros();    
+    count += 1;
+    if (count >= 3)
+    {
+      count = 0;
+      printSerialAngle();
+      //printAcc();
+      //printT();
+    }
+  }
+}
+
+void printAcc()
+{
+  Serial.print(aax);
+   Serial.print(",");
+   Serial.print(aay);
+   Serial.print(",");
+   Serial.print(aaz);
+   Serial.print(",");
+   Serial.println("E");
+}
+
+void printSerialAngle()
+{
+   Serial.print(angleXAcc);
+   Serial.print(",");
+   Serial.println(angleYAcc);
+}
+
+float AccelAdjust(int axis)
+{
+ float acc = 0;
+ for (int j=0;j<NADJ;j++)
+ {
+   float lectura=analogRead(axis);
+   acc = acc + ((lectura*5000)/1023.0);
+   delay(11); //número primo para evitar ciclos de lectura proporcionales
+ }
+ return acc/NADJ;
+}
---
+++ Arduino/GyroTest/GyroAngle1_40/GyroAngle1_40.ino
@@ -0,0 +1,516 @@
+#include <Wire.h>
+
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define CTRL_REG5 0x24
+
+
+#define STATUS_REG 0x27
+#define ZOR_REG 0b01000000
+#define ZDA_REG 0b00000100
+#define YOR_REG 0b00100000
+#define YDA_REG 0b00000010
+#define XOR_REG 0b00010000
+#define XDA_REG 0b00000001
+
+//use address 104 if CS is not connected
+int L3G4200D_Address = 105; //I2C address of the L3G4200D
+
+int zOld, xOld, yOld, xCand, yCand, zCand;
+int threshold = 200;
+
+float scale2000 = 70;
+
+float bx= 0,by=0,bz=0;
+long bxS,byS,bzS;
+
+
+// delta T control the routine frequency
+float deltaT = 5;
+float timerLoop = 0, timerReading = 0, timerSec = 0;
+float timerRoutine = 0, count = 0;
+float redingTime = 0, samplingTime = 0, calcTime =0, printTime = 0;
+float k=0, kM1=0, kMReading = 0, kMRoutine=0, kMLoop=0, secRoutine=0;
+
+//Boolean
+boolean processing = false;
+// Timer variables
+volatile int cont = 0;
+volatile int contSamples=0;
+volatile int contCalc=0;
+volatile float phi=0;
+volatile float theta=0;
+volatile float psi=0;
+volatile int x=0;
+volatile int y = 0;
+volatile int z= 0;
+volatile int dt=0;
+//volatile int dtM1=0;
+//volatile int dtM2=0;
+//volatile int dtM3=0;
+//volatile int dtM4=0;
+//volatile int dtM5=0;
+//volatile int dtM6=0;
+//volatile int dtM7=0;
+//volatile int dtM8=0;
+//volatile int dtM9=0;
+
+volatile int dtM1=0;
+volatile int dtM2=0;
+volatile int dtM3=0;
+volatile int dtM4=0;
+volatile int dtM5=0;
+volatile int yM1=0;
+volatile int yM2=0;
+volatile int yM3=0;
+volatile int yM4=0;
+volatile int yM5=0;
+volatile float thetaM1=0;
+volatile float thetaM2=0;
+volatile float thetaM3=0;
+volatile float thetaM4=0;
+volatile float thetaM5=0;
+volatile float thetaOLD = 0;
+volatile float thetaOLDM1=0;
+volatile float thetaOLDM2=0;
+volatile float thetaOLDM3=0;
+volatile float thetaOLDM4=0;
+volatile float thetaOLDM5=0;
+
+volatile float wF[3];
+boolean enFil = true;
+
+// Filter coeff
+volatile float alphaW = 1;
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(115200);
+  if (!processing)
+  {
+    Serial.println("starting up L3G4200D");
+    //Serial.println("Setting up timer3");
+  }
+
+
+  setupL3G4200D(2000); // Configure L3G4200  - 250, 500 or 2000 deg/sec
+  delay(1500); //wait for the sensor to be ready   
+  calcBias();
+  Serial.print("Readings take: (us)");
+  Serial.println(samplingTime);
+
+
+  // Initialize Timer
+  cli();
+  TCCR3A = 0;
+  TCCR3B = 0;
+
+  // Set compare match register to the desired timer count
+  //OCR3A=77; //16*10^6/(200Hz*1024)-1 = 77 -> 200 Hz 
+  OCR3A=193; //16*10^6/(80Hz*1024)-1 = 193 -> 80 Hz 
+  //OCR3A=780; //16*10^6/(20Hz*1024)-1 = 780 -> 20 Hz 
+  //OCR3A=50; //16*10^6/(308Hz*1024)-1 = 50 -> 308 Hz 
+
+  TCCR3B |= (1 << WGM32);
+  // Set CS10 and CS12 bits for 1024 prescaler:
+  TCCR3B |= (1 << CS30) | (1 << CS32);
+  // enable timer compare interrupt:
+  TIMSK3 |= (1 << OCIE3B);
+
+  // enable global interrupts:
+  sei(); 
+
+  //Serial.println("Interrupt enabled");
+
+  k = micros();
+}
+
+void loop()
+{
+  //SerialRoutine();
+  //timerLoop = micros()-kMLoop;
+  //kMLoop = micros();
+
+  timerSec = micros()-secRoutine;
+
+  if (timerSec >= 1000000)
+  {
+    secRoutine = micros();
+    Serial.print(cont);
+    Serial.print("    Smpl: ");
+    Serial.print(contSamples);
+    Serial.print("    calc: ");
+    Serial.println(contCalc);
+    /*
+    Serial.print("    wX: ");
+    Serial.print(wF[0]);
+    Serial.print("    Wy: ");
+    Serial.println(wF[1]);
+    Serial.print("    Wz: ");
+    Serial.println(wF[2]);
+    */
+    cont=0;      
+    contSamples=0;      
+    contCalc=0;      
+  }
+
+
+  timerRoutine = micros()-kMRoutine;
+  if (timerRoutine >= deltaT*1000)
+  {      
+    kMRoutine = micros();    
+    count += 1;
+    if (count >= 3)
+    {
+      count = 0;
+      printSerialAngle();
+      //printOmega();
+      //printT();
+    }
+  }
+}
+
+
+void wFilter(volatile float val[])
+{
+  val[0] = (1-alphaW)*x + alphaW*val[0];
+  val[1] = (1-alphaW)*y + alphaW*val[1];
+  val[2] = (1-alphaW)*z + alphaW*val[2];
+}
+
+void debug()
+{
+  Serial.print("thetaOld + y*dt = theta: ");
+  Serial.print("  ");
+  Serial.print(thetaOLD);
+  Serial.print(" + ");
+  Serial.print(y);
+  Serial.print("  ");
+  Serial.print(dt);
+  Serial.print(" =  ");
+  Serial.println(theta);
+  Serial.print("              ");
+  Serial.print("  ");
+  Serial.print(thetaOLDM1);
+  Serial.print(" + ");
+  Serial.print(yM1);
+  Serial.print("  ");
+  Serial.print(dtM1);
+  Serial.print(" =  ");
+  Serial.println(thetaM1);
+  Serial.print("             ");
+  Serial.print(thetaOLDM2);
+  Serial.print(" + ");
+  Serial.print(yM2);
+  Serial.print("  ");
+  Serial.print(dtM2);
+  Serial.print(" =  ");
+  Serial.println(thetaM2);
+  Serial.print("              ");
+  Serial.print(thetaOLDM3);
+  Serial.print(" + ");
+  Serial.print(yM3);
+  Serial.print("  ");
+  Serial.print(dtM3);
+  Serial.print(" =  ");
+  Serial.println(thetaM3);
+  Serial.print("              ");
+  Serial.print(thetaOLDM4);
+  Serial.print(" + ");
+  Serial.print(yM4);
+  Serial.print("  ");
+  Serial.print(dtM4);
+  Serial.print(" =  ");
+  Serial.println(thetaM4);
+  Serial.print("              ");
+  Serial.print(thetaOLDM5);
+  Serial.print(" + ");
+  Serial.print(yM5);
+  Serial.print("  ");
+  Serial.print(dtM5);
+  Serial.print(" =  ");
+  Serial.println(thetaM5);
+}
+
+ISR(TIMER3_COMPB_vect)
+{  
+  // Mettilo altrimenti non funziona
+  sei();
+  // Update x, y, and z with new values 2.5ms     
+  //wait until new z data available and no overrun
+  volatile byte statusflag = readRegister(L3G4200D_Address, STATUS_REG);
+  while(!(statusflag & ZDA_REG) && (statusflag & ZOR_REG)&&!(statusflag & YDA_REG) && (statusflag & YOR_REG)&& !(statusflag & XDA_REG) && (statusflag & XOR_REG)) 
+  {
+    statusflag = readRegister(L3G4200D_Address, STATUS_REG);
+  }
+  //read values
+  getGyroValues(); 
+  calcAngle();
+  cont++;
+}
+
+void SerialRoutine()
+{
+  if (Serial.available()>0)
+  {
+    byte in = Serial.read();
+    if (in=='A')
+    {
+      Serial.println("Ciao");
+    }
+    else if (in == 'B')
+    {
+      Serial.println("Bella");
+    }
+  }    
+}
+
+void calcAngle()
+{
+  dt = micros()-k;
+  if (!enFil)
+  {
+    phi=phi+(float)x*scale2000/1000*(float)dt/1000000.0;
+
+    thetaOLD = theta; 
+    theta=theta+(float)y*scale2000/1000* (float) dt/1000000.0;
+
+    psi=psi+z*(float)scale2000/1000* (float) dt/1000000.0; 
+  }
+  else if (enFil)
+  {  
+    phi=phi+wF[0]*(float)scale2000/1000* (float)dt/1000000.0;
+
+    thetaOLD = theta;
+    theta=theta+wF[1]*(float)scale2000/1000 *(float) dt/1000000.0;
+
+    psi=psi+wF[2]*(float)scale2000/1000* (float) dt/1000000.0;  
+  }
+
+  contCalc++;
+
+  dtM5=dtM4;
+  dtM4=dtM3; 
+  dtM3=dtM2;
+  dtM2=dtM1;
+  dtM1=dt;
+
+  yM5=yM4;
+  yM4=yM3; 
+  yM3=yM2;
+  yM2=yM1;
+  yM1=y;
+
+  thetaM5=thetaM4;
+  thetaM4=thetaM3; 
+  thetaM3=thetaM2;
+  thetaM2=thetaM1;
+  thetaM1=theta;
+
+  thetaOLDM5=thetaOLDM4;
+  thetaOLDM4=thetaOLDM3; 
+  thetaOLDM3=thetaOLDM2;
+  thetaOLDM2=thetaOLDM1;
+  thetaOLDM1=thetaOLD;
+
+  k=micros();  
+}
+
+void printT()
+{
+  Serial.print("  timer loop: "); 
+  Serial.print(timerLoop);
+  Serial.print("  timer reading: "); 
+  Serial.print(timerReading);
+  Serial.print("  timer routine: "); 
+  Serial.print(timerRoutine);
+  //Serial.print("  Calc time: "); 
+  //Serial.print(calcTime);
+  //Serial.print("  Sampling time: "); 
+  //Serial.print(samplingTime);
+  Serial.print("  dt: "); 
+  Serial.println(dt);
+}
+
+void getGyroValues()
+{  
+
+  //starting samplingTimer
+  //samplingTime = micros();
+
+  byte xMSB = readRegister(L3G4200D_Address, 0x29);
+  byte xLSB = readRegister(L3G4200D_Address, 0x28);
+  x = ((xMSB << 8) | xLSB);
+
+  byte yMSB = readRegister(L3G4200D_Address, 0x2B);
+  byte yLSB = readRegister(L3G4200D_Address, 0x2A);
+  y = ((yMSB << 8) | yLSB);
+
+  byte zMSB = readRegister(L3G4200D_Address, 0x2D);
+  byte zLSB = readRegister(L3G4200D_Address, 0x2C);
+  z = ((zMSB << 8) | zLSB);
+
+  removeBias();
+
+  if (enFil)
+  {
+    wF[0] = x;
+    wF[1] = y;
+    wF[2] = z;
+    wFilter(wF);
+  }
+
+  //samplingTime = micros()- samplingTime;
+  contSamples++;
+}
+
+
+void writeRegister(int deviceAddress, byte address, byte val) 
+{
+  Wire.beginTransmission(deviceAddress); // start transmission to device 
+  Wire.write(address);       // send register address
+  Wire.write(val);         // send value to write
+  Wire.endTransmission();     // end transmission
+}
+
+int readRegister(int deviceAddress, byte address)
+{
+  int v;
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(address); // register to read
+  Wire.endTransmission();
+
+  Wire.requestFrom(deviceAddress, 1); // read a byte
+
+  while(!Wire.available()) 
+  {
+    // waiting
+    // Serial.println("No Data");
+  }
+
+  v = Wire.read();
+  return v;
+}
+
+void printOmega()
+{
+  Serial.print("       Wx:");
+  Serial.print(wF[0]);
+
+  Serial.print("       Wy:");
+  Serial.print(wF[1]);
+
+  Serial.print("       Wz:");
+  Serial.print(wF[2]);
+  Serial.print("      wx:");
+  Serial.print(x);
+
+  Serial.print("       wy:");
+  Serial.print(y);
+
+  Serial.print("       Wz:");
+  Serial.println(z);
+}
+
+void printAngle()
+{
+  Serial.print("Phi:");
+  Serial.print(phi);
+
+  Serial.print("       Theta:");
+  Serial.print(theta);
+
+  Serial.print("       Psi:");
+  Serial.println(psi);
+}
+
+void printSerialAngle()
+{
+  Serial.print(phi);
+  Serial.print(",");
+  Serial.print(theta);
+  Serial.print(",");
+  Serial.println(psi);
+}
+
+void calcBias()
+{  
+  if (!processing)
+    Serial.println("Bias");
+  int c = 2000;
+  for (int i = 0; i<c; i++)
+  {
+    getGyroValues(); 
+    bxS = bxS + x;
+    byS = byS + y;
+    bzS = bzS + z;
+  }
+
+  bx = bxS / c;
+  by = byS / c;
+  bz = bzS / c;
+
+  if (!processing)
+  {
+    Serial.println(bx);
+    Serial.println(by);
+    Serial.println(bz);
+  }  
+}
+
+void removeBias()
+{
+  x = x - bx;
+  y = y - by;
+  z = z - bz; 
+} 
+
+int setupL3G4200D(int scale)
+{
+  //From  Jim Lindblom of Sparkfun's code
+
+    // Enable x, y, z and turn off power down:
+  //writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
+  // 400Hz and 1Hz cutoff frq of the HPF
+  //writeRegister(L3G4200D_Address, CTRL_REG1, 0b01011111);
+  // 200Hz 
+  writeRegister(L3G4200D_Address, CTRL_REG1, 0b01001111);
+
+  // If you'd like to adjust/use the HPF:
+  // High pass filter cut off frecuency configuration
+  // ODR 200Hz, cutoff 0.02Hz 1001
+  //writeRegister(L3G4200D_Address, CTRL_REG2, 0b00001001);
+  // ODR 200Hz, cutoff 1 Hz 0100
+  //writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000100);
+  // ODR 200Hz, cutoff 0.02Hz 1001
+  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00001001);
+
+  // Configure CTRL_REG3 to generate data ready interrupt on INT2
+  // No interrupts used on INT1, if you'd like to configure INT1
+  // or INT2 otherwise, consult the datasheet:
+  //writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
+
+  // CTRL_REG4 controls the full-scale range, among other things:
+
+  if(scale == 250)
+  {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00000000);
+  }
+  else if(scale == 500)
+  {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00010000);
+  }
+  else
+  {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00110000);
+  }
+
+  // CTRL_REG5 controls high-pass filtering of outputs, use it
+  // if you'd like:
+  writeRegister(L3G4200D_Address, CTRL_REG5, 0b00010000);
+  //writeRegister(L3G4200D_Address, CTRL_REG5, 0b00000000);
+}
+
