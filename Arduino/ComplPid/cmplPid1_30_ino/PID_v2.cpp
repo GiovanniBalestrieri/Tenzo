92d2f1a7e47ceba74614b5f5d5e43f20f93dd26b
UserK <balestrieri.gepp@gmail.com>
2015-04-06 22:54:35 +0200

    test pid library

--- Arduino/ComplPid/cmplPid1_30_ino/PID_v2.cpp
+++ Arduino/ComplPid/cmplPid1_30_ino/PID_v2.cpp
@@ -21,20 +21,20 @@ PID::PID(double* Input, double* Output, double* Setpoint,
         double Kp, double Ki, double Kd, int ControllerDirection)
 {
 	
-    myOutput = Output;
-    myInput = Input;
-    mySetpoint = Setpoint;
-	  inAuto = false;
+    volatile myOutput = Output;
+    volatile myInput = Input;
+    volatile mySetpoint = Setpoint;
+	  volatile inAuto = false;
 	
 	PID::SetOutputLimits(0, 255);				//default output limit corresponds to 
 												//the arduino pwm limits
 
-    SampleTime = 100;							//default Controller Sample Time is 0.1 seconds
+    volatile SampleTime = 100;							//default Controller Sample Time is 0.1 seconds
 
     PID::SetControllerDirection(ControllerDirection);
     PID::SetTunings(Kp, Ki, Kd);
 
-    lastTime = millis()-SampleTime;				
+    volatile lastTime = millis()-SampleTime;				
 }
  
  
