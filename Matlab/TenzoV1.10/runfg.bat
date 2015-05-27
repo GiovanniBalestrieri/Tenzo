c0bbf7b337044f560dba392edfac61560ab8f6d5
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-05-27 14:13:33 +0200

    [Org] Matlab

--- Matlab/TenzoV1.10/runfg.bat
+++
@@ -1,5 +0,0 @@
-C:
-cd C:\Program Files\FlightGear
-
-SET FG_ROOT=C:\Program Files\FlightGear\data
-.\\bin\win32\fgfs --aircraft=777-200 --fdm=network,localhost,5501,5502,5503 --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --enable-freeze --airport=KSFO --runway=10L --altitude=7224 --heading=113 --offset-distance=4.72 --offset-azimuth=0
