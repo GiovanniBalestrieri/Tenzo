7360900d580f9bfaa5f73c87d4ef1a7a43aed8d7
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-07-11 16:13:44 +0200

    Added Crs exercise folder

---
+++ Matlab/Crs/NewFolder/untitled3.m
@@ -0,0 +1,17 @@
+%% Nyquist plot
+figure(1); clf reset
+G=tf(3,[1 3 2])
+nyquist(G)
+
+%% Different Plant tf and Nyquist plots
+figure(2);
+
+G1 = tf(3*[3 1],[1 3 2]);
+G2 = tf(3*[2/3 1],[1 3 2]);
+G3 = tf(3*[0.1 1],[1 3 2]);
+
+bode(G1,G2,G3);
+nyquist(G1,G2,G3)
+a = legend('1','2','3')
+set(a,'font
+
