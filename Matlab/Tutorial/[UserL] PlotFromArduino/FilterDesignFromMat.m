c8dee12d1b59e191ec10fd05d03f5fffda7d916d
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-08-06 19:40:00 +0200

    Updated Gyro after Stack Question

--- Matlab/Tutorial/[UserL] PlotFromArduino/FilterDesignFromMat.m
+++ Matlab/Tutorial/[UserL] PlotFromArduino/FilterDesignFromMat.m
@@ -8,18 +8,20 @@ gzFdata = 0;
 % Loads samples from file
 dat = load('GyroSamples.mat');
 dat = load('samples.mat');
+%datX100 = dat.x(1:100)
+%plot(datX100)
 
 % Plots raw data vs samples
 figure(2);
 plot(dat.x);
-title('Noisy acceleration along X axis');
-xlabel('Samples [s]');
-ylabel('Amplitude [m*s^-2]');
+title('Angular velocity along X axis');
+xlabel('Samples');
+ylabel('Amplitude [deg*s^-1]');
 grid on
 grid minor
 
 
-% Plots magnitude spectrum of the signal
+%% Plots magnitude spectrum of the signal
 X_mags=abs(fft(dat.x));
 figure(3)
 plot(X_mags);
@@ -29,41 +31,57 @@ ylabel('Magnitude');
 % Plots first half of DFT (normalized frequency)
 num_bins = length(X_mags);
 plot([0:1/(num_bins/2 -1):1],X_mags(1:num_bins/2))
+grid on
 xlabel('Normalized frequency [\pi rads/samples]');
 ylabel('Magnitude');
 
-% Designs a second order filter using a butterworth design guidelines
-[b a] = butter(2,0.5,'high');
+% Normalize X_mags
 
+X_magsNorm = (X_mags - min(X_mags)) / ( max(X_mags) - min(X_mags) )
+
+%% Designs a second order filter using a butterworth design guidelines
+[b a] = butter(2,0.005,'high');
+
+figure(4);
 % Plot the frequency response (normalized frequency)
 H = freqz(b,a,floor(num_bins/2));
-hold on
 plot([0:1/(num_bins/2 -1):1], abs(H), 'r');
+hold on
+grid on
+plot(0:1/(num_bins/2 -1):1,X_magsNorm(1:num_bins/2),'g')
 
 % Filters the signal using coefficients obtained by the butter filter
 % design
+datX = dat.x';
 x_filtered = filter(b,a,dat.x);
 
 % Plots the filtered signal
-figure(4)
+figure(5)
 plot(x_filtered,'r')
+hold on
+plot(dat.x,'b')
 title('Filtered Signal - Second Order Butterworth');
 xlabel('Samples');
 ylabel('Amplitude');
 
-% Redesign the filter using a higher order filter
+%% Redesign the filter using a higher order filter
 [b2 a2] = butter(3,0.5,'high');
 
 % Plots the magnitude spectrum and compare with lower order
 H2 = freqz(b2,a2,floor(num_bins/2));
-figure(3);
+figure(6);
 hold on
 plot([0:1/(num_bins/2 -1):1], abs(H2), 'r');
+hold on
+grid on
+plot(0:1/(num_bins/2 -1):1,X_magsNorm(1:num_bins/2),'g')
 
 %filter the noisy signal and plots the result
 x_filtered2 = filter(b2,a2,dat.x);
-figure(5)
+figure(7)
 plot(x_filtered2,'g');
+hold on
+plot(dat.x,'b')
 title('Filtered Signal - 20 Order Butterworth');
 xlabel('Samples');
 ylabel('Amplitude');
@@ -99,8 +117,8 @@ for i = 1:num_bins
 %     gzFdata = [ gzFdata(2:end) ; gzFilt ];
 end
 
-figure(6)
-plot(dat.x,'r--*');
+figure(8)
+plot(dat.x,'r--');
 hold on
 plot(gxFdata,'b');
 hold on
--- Matlab/Tutorial/[UserL] PlotFromArduino/filterBasicsHP.m
+++ Matlab/Tutorial/[UserL] PlotFromArduino/filterBasicsHP.m
@@ -15,11 +15,14 @@ t = 0:1/Fs:1;
 
 % Generate random values [min,max] = [xm,xM]
 xm = 2;
-xM = 20;
+xM = 8;
 amp = xm+ (xM-xm).*rand(1,1);
 
 % Use random value for the amplitude
 y = amp*sin(2*pi*freq*t) + bias;
+
+yEasy = y - mean(y);
+
 y0 = 0;
 y18 = 18;
 % Plots raw data vs samples
@@ -29,6 +32,8 @@ hold on
 plot(t,y0,'k','LineWidth',2);
 hold on
 plot(t,y18,'k');
+hold on
+plot(t,yEasy,'k');
 title('Sinusoid');
 xlabel('Time [s]');
 ylabel('Amplitude of f');
@@ -46,7 +51,7 @@ ylabel('Magnitude');
 
 % Plots first half of DFT (normalized frequency)
 num_bins = length(X_mags);
-plot([0:1/(num_bins/2 -1):1],X_mags(1:num_bins/2))
+plot(0:1/(num_bins/2 -1):1,X_mags(1:num_bins/2));
 grid on
 xlabel('Normalized frequency [\pi rads/samples]');
 ylabel('Magnitude');
@@ -68,7 +73,7 @@ hold on
 grid on
 plot(0:1/(num_bins/2 -1):1,X_magsNorm(1:num_bins/2),'g')
 
-% Filters the signal using coefficients obtained by the butter filter
+%% Filters the signal using coefficients obtained by the butter filter
 % design
 x_filtered = filter(b,a,y);
 
@@ -90,25 +95,29 @@ ylabel('Amplitude');
 Fs = 350;
 % Sinusoid frequency
 freq = 30;
-bias = 15
-t = 0:1/Fs:1;
+bias = 1
+maxTime = 2;
+t = 0:1/Fs:maxTime;
 
 % Generate random values [min,max] = [xm,xM]
-xm = 2;
-xM = 20;
-amp = xm+ (xM-xm).*rand(1,1);
+xm = -0.1;
+xM = 0.1;
+amp = xm+ (xM-xm).*rand(1,Fs*maxTime+1);
 
 % Use random value for the amplitude
-y = amp*sin(2*pi*freq*t) + bias;
+y = bias + amp;
 y0 = 0;
-y18 = 18;
+ybias = bias;
+y2 = 2;
 % Plots raw data vs samples
-figure(1);
+figure(5);
 plot(t,y);
 hold on
 plot(t,y0,'k','LineWidth',2);
 hold on
-plot(t,y18,'k');
+plot(t,y2,'k');
+hold on
+plot(t,ybias,'r');
 title('Sinusoid');
 xlabel('Time [s]');
 ylabel('Amplitude of f');
@@ -119,7 +128,7 @@ grid minor
 
 % Plots magnitude spectrum of the signal
 X_mags=abs(fft(y));
-figure(2)
+figure(6)
 plot(X_mags);
 xlabel('DFT Bins');
 ylabel('Magnitude');
@@ -141,7 +150,7 @@ X_magsNorm = (X_mags - min(X_mags)) / ( max(X_mags) - min(X_mags) )
 [b a] = butter(2,0.2,'high');
 
 % Plot the frequency response (normalized frequency)
-figure(3)
+figure(7)
 H = freqz(b,a,floor(num_bins/2));
 plot(0:1/(num_bins/2 -1):1, abs(H), 'r');
 hold on
@@ -153,12 +162,12 @@ plot(0:1/(num_bins/2 -1):1,X_magsNorm(1:num_bins/2),'g')
 x_filtered = filter(b,a,y);
 
 % Plots the filtered signal
-figure(4)
+figure(8)
 plot(t,x_filtered,'r')
 hold on 
 plot(t,y0,'k','LineWidth',2)
 hold on 
-plot(t,15,'k','LineWidth',2)
+plot(t,ybias,'k','LineWidth',2)
 grid on
 title('Filtered Signal - Second Order Butterworth');
 xlabel('Samples');
