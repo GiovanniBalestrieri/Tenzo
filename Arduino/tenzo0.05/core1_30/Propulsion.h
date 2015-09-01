2f60aac2439284e6a08d888adde7601a43489d17
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-09-02 01:15:03 +0200

    Added 1.30 - Control loop @ 80 Hz - Testing loop aroud angular velocity

---
+++ Arduino/tenzo0.05/core1_30/Propulsion.h
@@ -0,0 +1,62 @@
+/*
+  propulsion.h - Library to calibrate ESC's, arm, test, spin 
+  and stop your Brushless motors.
+  Created by Giovanni Balestrieri - UserK, August 25, 2015.
+*/
+#ifndef Propulsion_h
+#define Propulsion_h
+
+#include "Arduino.h"
+#include <Servo.h>
+#include "Ux.h"
+
+
+// Constants
+const int MAX_SIGNAL = 2000;
+const int MIN_SIGNAL = 700;
+const int IDLE_THRESHOLD = 870;
+
+class Propulsion
+{
+  public:
+        Propulsion(int, int, int, int);
+        void init();
+        void calibrateOnce();
+ 	void calibrateAgain();
+	void resetMotors();
+        void test();
+        void stopAll();
+	int initialize();
+	void setSpeeds(int, float, float, float, float);
+        void setSpeedWUs(int);
+        void setSpeedradS(int);
+        void detachAll();
+	void idle(); 
+
+        int getwUs1();
+        int getwUs2();
+        int getwUs3();
+        int getwUs4();
+
+
+        int getThrottle();
+        void setThrottle(int);	 
+
+        Servo servo1;
+	Servo servo2;
+	Servo servo3;
+	Servo servo4;
+
+        int wUs1;
+        int wUs2;
+        int wUs3;
+        int wUs4;
+
+	int throttle = 700;
+  private:
+        int _pinM1;
+        int _pinM2;
+        int _pinM3;
+        int _pinM4;
+};
+#endif
