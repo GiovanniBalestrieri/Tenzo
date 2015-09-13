7f669c0f594cf216d6aaba8b388e4f5614982a45
balestrieri.gepp@gmail.com <balestrieri.gepp@gmail.com>
2015-09-13 22:58:04 +0200

    Added serial trick python

--- Arduino/MPU6050/Basic/Ros/serDemo.py
+++ Arduino/MPU6050/Basic/Ros/serDemo.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 import serial
 import sys
 import time
 
 port = '/dev/ttyACM6'
+portRfid = '/dev/ttyUSB0'
 
 try:
-        #ser = serial.Serial(port,115200);
-	ser = serial.Serial('/dev/ttyACM6',115200)
+        #ser = serial.Serial(port,115200)
+	ser = serial.Serial('/dev/ttyUSB0',2400)
 except:
 	print "Unable to open serial port" + port       
 
