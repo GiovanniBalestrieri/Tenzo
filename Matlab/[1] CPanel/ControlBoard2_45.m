0b806b084e9a22fd31617a63f7f26983e40adfa4
UserK <balestrieri.gepp@gmail.com>
2016-03-09 16:18:42 +0100

    Solved Connection problem: Added check whether the xbee serial object exists and has been created Solved Disconnection issue

--- Matlab/[1] CPanel/ControlBoard2_45.m
+++ Matlab/[1] CPanel/ControlBoard2_45.m
@@ -1683,8 +1683,19 @@ Listener = addlistener(hTabGroup,'SelectedTab','PostSet',@tabGroupCallBack);
             end
 
             %  Setting up serial communication
-            %  If the xbee variable existes but it is empty recreate it
-            if (exist('xbee','var') || isempty(xbee))
+            %  If the xbee variable doesn't exist, create it
+            if (~exist('xbee','var'))
+                xbee = serial(portUnix,'baudrate',xbeeBR,'tag',tag);
+                %xbee = serial(portWin,'baudrate',xbeeBR,'terminator',terminator,'tag',tag);
+
+                % Max wait time
+                set(xbee, 'TimeOut', 10);  
+                % One message long buffer
+                set(xbee, 'InputBufferSize',inputBuffSize)
+                % Open the serial
+                fopen(xbee);    
+            elseif (exist('xbee','var') || isempty(xbee))
+                %if the xbee serial object exists but it is empty -> recreate it
                 xbee = serial(portUnix,'baudrate',xbeeBR,'tag',tag);
                 %xbee = serial(portWin,'baudrate',xbeeBR,'terminator',terminator,'tag',tag);
 
