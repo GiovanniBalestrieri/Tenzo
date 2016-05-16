7a5c158884f2975207125c1ad19463e9a9202577
userk <balestrieri.gepp@gmail.com>
2016-05-16 22:37:08 +0200

    Ok quadrature encoders with 2 interrupt pins

--- Arduino/encoders/encodersMicro/encodersMicro.ino
+++ Arduino/encoders/encodersMicro/encodersMicro.ino
@@ -1,48 +1,50 @@
-// Define encoder pin Interrupt
-#define pinEncoderBS 5
+//From bildr article: http://bildr.org/2012/08/rotary-encoder-arduino/
 
-volatile int MSverso = 0;
-volatile long int SX_Enc_Ticks = 0;
-volatile boolean SX_Enc_BSet2;
-volatile boolean SX_Enc_BSet3;
+//these pins can not be changed 2/3 are special pins
+int encoderPin1 = 2;
+int encoderPin2 = 3;
+
 volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
 
+long lastencoderValue = 0;
 
-const byte interruptPin2 = 2; // digital pin 0
-const byte interruptPin3 = 3; // digital pin 1
+int lastMSB = 0;
+int lastLSB = 0;
 
-void setup()
-{
-  pinMode(interruptPin2, INPUT_PULLUP);
-  pinMode(interruptPin3, INPUT_PULLUP);
-  
-  //attachInterrupt(interruptPin2, MSencVel, RISING);
-  //attachInterrupt(interruptPin3, MSencVelFall, FALLING);
-  
-  attachInterrupt(interruptPin2, updateEncoder, CHANGE);   
-  attachInterrupt(interruptPin3, updateEncoder, CHANGE); 
-  Serial.begin(115200);
+void setup() {
+  Serial.begin (9600);
+
+  pinMode(encoderPin1, INPUT_PULLUP); 
+  pinMode(encoderPin2, INPUT_PULLUP);
+
+  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
+  attachInterrupt(2, updateEncoder, CHANGE); 
+  attachInterrupt(3, updateEncoder, CHANGE);
+
 }
 
+void loop(){ 
+  //Do stuff here
 
-
-void loop(){
-  //if (SX_Enc_BSet)
-    Serial.println(SX_Enc_Ticks);
+  Serial.println(encoderValue);
+  delay(1000); //just here to slow down the output, and show it will work  even during a delay
 }
+
 
 void updateEncoder(){
-  int MSB = digitalRead(interruptPin2); //MSB = most significant bit
-  int LSB = digitalRead(interruptPin3); //LSB = least significant bit
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
 
   int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
   int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
 
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) 
-    SX_Enc_Ticks ++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) 
-    SX_Enc_Ticks --;
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
 
   lastEncoded = encoded; //store this value for next time
 }
-
