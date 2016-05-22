d4beca5acdff0da39c783528f5c9f9c3d972dfcc
userk <balestrieri.gepp@gmail.com>
2016-05-23 00:11:25 +0200

    Added global header

---
+++ Arduino/tenzo0.10/core2_00/tenzo.h
@@ -0,0 +1,19 @@
+#ifndef tenzo_h
+#define  tenzo_h
+
+
+#include "Propulsion.h"
+#include "Ux.h"
+#include "NonLinearPid.h"
+#include "ControlPid.h"
+#include "FlightParams.h"
+#include "MedianFilter.h"
+#include "FreeSixIMU.h"
+#include "FIMU_ADXL345.h"
+#include "FIMU_ITG3200.h"
+#include "tenzo_timer.h"
+#include "sched.h"
+#include "task.h"
+
+
+#endif
