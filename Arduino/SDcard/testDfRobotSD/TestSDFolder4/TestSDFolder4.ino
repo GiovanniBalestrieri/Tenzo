282277070ee05c06d4acd8e16a9cbce6b5de1afc
userk <balestrieri.gepp@gmail.com>
2016-06-04 18:22:58 +0200

    Added RTC feature

--- Arduino/SDcard/testDfRobotSD/TestSDFolder4/TestSDFolder4.ino
+++ Arduino/SDcard/testDfRobotSD/TestSDFolder4/TestSDFolder4.ino
@@ -1,8 +1,16 @@
 // include the SD library:
 #include <SD.h>
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 RTC;
 
 File myFile;
 File myFile1;
+File my1;
+File my2;
+File my3;
+
+File folder;
 
 uint8_t sd_answer;
  
@@ -14,6 +22,7 @@ const int chipSelect = 4;
 char testFile[] = "file1.txt";
 char exFile[] = "file2.txt";
 
+char path1[] = "arduino/file1";
 char output[10];
 
 int cont = 0;
@@ -22,7 +31,11 @@ void setup()
 {
  // Open serial communications and wait for port to open:
   Serial.begin(115200); 
- 
+  Wire.begin();
+  RTC.begin();
+  //If we remove the comment from the following line, we will set up the module time and date with the computer one
+  RTC.adjust(DateTime(__DATE__, __TIME__));
+  
   Serial.print("\nInitializing SD card...");
   pinMode(53, OUTPUT); 
  
@@ -35,6 +48,13 @@ void setup()
   } else {
    Serial.println("Wiring is correct and a card is present."); 
   }
+
+  
+  folder = SD.open("/LOG");
+  //root =
+  //folder.ls(LS_R | LS_DATE | LS_SIZE);
+  printDirectory(folder,0);
+  folder.close();
 
   if (SD.exists(exFile))
   {
@@ -57,13 +77,82 @@ void setup()
       Serial.println("\n.\n");
     myFile.close();
   }  
+  
+  if (SD.exists(path1))
+  {
+    Serial.println("file2 exists. Removing...");  
+    SD.remove(path1);      
+  } else {
+    Serial.println("file1 DOES NOT exist");    
+  }
+  my1 = SD.open(path1, FILE_WRITE); 
+  //my2 = SD.open(path2, FILE_WRITE); 
+  //my3 = SD.open(path3, FILE_WRITE); 
+  my1.println("init 1");
+  //my2.println("init 2");
+  //my3.println("init 3");
+    my1.close();
+    //my2.close();
+    //my3.close();
+
+ 
+  
+  folder = SD.open("/arduino");
+  //root =
+  //folder.ls(LS_R | LS_DATE | LS_SIZE);
+  printDirectory(folder,0);
+  folder.close();
+  
+}
+
+void printDirectory(File dir, int numTabs)
+{
+  while (true) {
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
 }
  
- 
 void loop(void) {   
-  myFile = SD.open(exFile,FILE_WRITE);;
-  myFile.print(cont);
+
+  DateTime now = RTC.now();
+
+  myFile = SD.open(path1,FILE_WRITE);
+  myFile.print(now.day());
+  myFile.print('/');
+  //We print the month
+  myFile.print(now.month());
+  myFile.print('/');
+  //We print the year
+  myFile.print(now.year());
+  myFile.print(' ');
+  //We print the hour
+  myFile.print(now.hour());
+  myFile.print(':');
+  //We print the minutes
+  myFile.print(now.minute());
+  myFile.print(':');
+  //We print the seconds
+  myFile.print(now.second());
+  myFile.println();
   myFile.close();
+  
   
   Serial.println("Reading file2.txt...");
   myFile = SD.open(exFile);
@@ -74,6 +163,7 @@ void loop(void) {
     }
     myFile.close();
   }
+ 
  
   delay(1000);
   Serial.println("END\n");
