7e3ea053d71e683a1751a6b95a4884c88914188d
userk <balestrieri.gepp@gmail.com>
2016-07-24 00:35:23 +0200

    Updated Euler Equation solver for Generic Functional/Performance measure

--- python/optimization/minimize_functional.py
+++ python/optimization/minimize_functional.py
@@ -2,8 +2,10 @@
 
 from math import *
 from sympy import *
+import numpy as np
 import scipy.integrate as integrate
 from scipy.optimize import minimize_scalar,minimize
+import matplotlib.pyplot as plt
 
 # We want to find the optimal curve of minimium length that connects point X0 to Xf 
 
@@ -11,15 +13,29 @@ from scipy.optimize import minimize_scalar,minimize
 # [ a11 a12]  =  [  0  1 ]
 # [ a21 a22]     [ -1 -2 ]
 
+a11 = 0
+a12 = 1
+a21 = -1
+a22 = -2
+
 def pend(X, t, a11, a12, a21, a22):
      dydt = [a11*X[0] + a12*X[1], a21*X[0] + a22*X[1]]
      return dydt
 
 
 def main():
-    a_t = np.arange(0, 25.0, 0.01)
-    asol = integrate.odeint(pend, [1, 0], a_t)
-    print(asol)
+	a_t = np.arange(0, 25.0, 0.01)
+	asol = integrate.odeint(pend, [1, 0], a_t, args=(a11,a12,a21,a22))
+	print(asol)
+	plt.plot(a_t, asol[:, 0], 'b', label='theta(t)')
+	plt.plot(a_t, asol[:, 1], 'g', label='omega(t)')
+	plt.legend(loc='best')
+	plt.xlabel('t')
+	plt.grid()
+	plt.show()	
+
+if __name__ == '__main__':
+    main()
 
 #cons = ({'type': 'ineq', 'fun': lambda x:  x[0] - 2 * x[1] + 2},{'type': 'ineq', 'fun': lambda x: -x[0] - 2 * x[1] + 6},{'type': 'ineq', 'fun': lambda x: -x[0] + 2 * x[1] + 2})
 
