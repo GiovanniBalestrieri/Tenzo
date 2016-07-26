d57567bbc3626a98bc777598b78464c591a1f0ce
userk <balestrieri.gepp@gmail.com>
2016-07-26 18:25:26 +0200

    Funziona!

--- python/optimization/ex4-3-1.py
+++ python/optimization/ex4-3-1.py
@@ -8,31 +8,31 @@ from scipy.optimize import minimize_scalar,minimize
 import matplotlib.pyplot as plt
 
 def pend(X, t):
-     dydt = [X[0]*X[1]*X[3]/(X[3]*t**2+X[3]**2) -4*t*X[2]**2/(2*(X[2]*t**2+X[3]**2)) - X[3]*X[0]**2*X[2]/(2*(X[2]*t**2+X[3]**2)),-X[1]**2/2*X[2]-X[3]/(X[2]**2*t**2+X[2]*X[3]**2)*((X[0]*X[1]*X[2]**2)/2-2*t*X[2]**2-(X[3]*X[0]**2*X[2])/2) ]
+     dydt = [X[2],X[3], X[0]*X[1]*X[3]/(X[3]*t**2+X[3]**2) -4*t*X[2]**2/(2*(X[2]*t**2+X[3]**2)) - X[3]*X[0]**2*X[2]/(2*(X[2]*t**2+X[3]**2)),-X[1]**2/2*X[2]-X[3]/(X[2]**2*t**2+X[2]*X[3]**2)*((X[0]*X[1]*X[2]**2)/2-2*t*X[2]**2-(X[3]*X[0]**2*X[2])/2) ]
      return dydt
 
 
 def main():
 	#a_t = np.arange(0, 25.0, 0.01)
-	T = np.mgrid[-6:7,0:10:0.1]
-	X1 = np.linspace(0,10,100)
-	X2 = np.linspace(0,10,100)
-	X3 = np.linspace(0,10,100)
-	X4 = np.linspace(0,10,100)
-	XX = np.array([X1,X2,X3,X4])
-	print(XX)
-	print(T[1])
-	print(T[0])
+	X1 = np.linspace(0,2,101)
+	#T = np.array([X1,X2,X3,X4])
+	#print(X1)
+	X0 = [1,1,1,1]
+	print(X0)
+	print(pend(X0,1))
+	
 	#a_t = np.mgrid[0:10.0:0.01,0:10:0.01,0:10:0.01,0:10:0.01]  
 	#print(a_t[0])
-	#asol = integrate.odeint(pend, [1,1,0,0], a_t)
+	asol = integrate.odeint(pend,[0,0,1,1], X1)
 	#print(asol)
-	#plt.plot(a_t, asol[:, 0], 'b', label='theta(t)')
-	#plt.plot(a_t, asol[:, 1], 'g', label='omega(t)')
+	plt.plot(X1, asol[:, 0], 'b', label='X(t)')
+	plt.plot(X1, asol[:, 1], 'g', label='Y(t)')
+	plt.plot(X1, asol[:, 2], 'b*', label='vX(t)')
+	plt.plot(X1, asol[:, 3], 'g*', label='vY(t)')
 	plt.legend(loc='best')
 	plt.xlabel('t')
 	plt.grid()
-	#plt.show()	
+	plt.show()	
 
 if __name__ == '__main__':
     main()
