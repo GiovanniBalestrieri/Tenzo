a3c93d46e34ecf9865f276fe0f43d0324829001a
userk <balestrieri.gepp@gmail.com>
2016-08-25 01:11:13 +0200

    Naive bayes algorithm for text classification

--- python/optimization/nltk/textClassification/textClas.py
+++ python/optimization/nltk/textClassification/textClas.py
@@ -7,28 +7,47 @@ documents=[(list(movie_reviews.words(fileid)),category)
 	for category in movie_reviews.categories()
 	for fileid in movie_reviews.fileids(category)]
 
-
 #for category in movie_reviews.categories():
 #	for fileid in movie_reviews.fileids(category):
 #		documents.append(list(movie_reviews.words(fileid)),category)
 
-
 random.shuffle(documents)
-
 
 # Prints all the words in the reviews collection and its label pos/neg
 #print(documents[2])
-
 
 words = []
 # Converto to lower case
 for w in movie_reviews.words():
 	words.append(w.lower())
 
-
 # convert to nltk frequency distribution
 all_words = nltk.FreqDist(words)
-print(all_words.most_common(15))
 
+# Show most common words
+print(all_words.most_common(15))
+# See how many times the word stupid is present in the reviews
 print(all_words["stupid"])
 
+word_feature = list(all_words.keys())[:1000]
+
+def find_feature(document):
+	words = set(document)
+	features = {}
+	for w in word_feature:
+		features[w] = (w in words)
+	return features
+
+
+print(find_feature(movie_reviews.words('neg/cv000_29416.txt')))
+
+featuresets = [(find_feature(rev),category) for (rev,category) in documents]
+
+# Define Training and Test Set
+training_set = featuresets[:1900]
+test_set = featuresets[1900:]
+
+# Train with Naive Bayes
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("Naive Bayes Alg acc:", (nltk.classify.accuracy(classifier,test_set))*100)
+classifier.show_most_informative_features(15)
