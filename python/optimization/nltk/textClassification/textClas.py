3b8644eb054ad17188d28949dad5e03bf2887bb3
userk <balestrieri.gepp@gmail.com>
2016-09-09 02:04:13 +0200

    Ok Voting Classifie TODO:: check por positive and negative performance

--- python/optimization/nltk/textClassification/textClas.py
+++ python/optimization/nltk/textClassification/textClas.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import nltk
 import random
 from nltk.corpus import movie_reviews
@@ -20,20 +21,23 @@ class VoteClassifier(ClassifierI):
 			v = c.classify(features)
 			votes.append(v)
 
-		return mode(votes)
+		modeVotes = max(set(votes), key=votes.count)
+		return modeVotes
 
 	def confidence(self,features):
 		votes = []
 		for c in self._classifiers:
 			v = c.classify(features)
 			votes.append(v)
-
-		choice_votes = votes.count(mode(votes))
-		print(votes)
-		print(mode(votes))
-		print(choice_votes)
-		print(len(votes))
-		conf = choice_votes*100 / len(votes)
+		
+		modeVotes = max(set(votes), key=votes.count)
+	        
+		choice_votes = votes.count(modeVotes)
+		tot = len(votes)
+		conf = choice_votes/tot
+		#print(votes)
+		#print(len(votes))
+		#print("Confidence: ", choice_votes)
 		return conf
 
 
@@ -64,7 +68,7 @@ print(all_words.most_common(15))
 # See how many times the word stupid is present in the reviews
 print(all_words["really"])
 
-word_feature = list(all_words.keys())[:5000]
+word_feature = list(all_words.keys())[:3000]
 
 def find_feature(document):
 	words = set(document)
@@ -83,34 +87,46 @@ training_set = featuresets[:1900]
 test_set = featuresets[1900:]
 
 # Train with Naive Bayes
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-#classifer_f = open("naiveBayes.pickle","rb")
-#classfier = pickle.load(classifier_f)
-#classifier_f.close()
+#classifier = nltk.NaiveBayesClassifier.train(training_set)
+classifier_f = open("naiveBayes.pickle","rb")
+classifier = pickle.load(classifier_f)
+classifier_f.close()
 
 print(" Original Naive Bayes Alg acc:", (nltk.classify.accuracy(classifier,test_set))*100)
 classifier.show_most_informative_features(15)
 
-save_classifier = open("naiveBayes.pickle","wb")
-pickle.dump(classifier,save_classifier)
-save_classifier.close()
+#save_classifier = open("naiveBayes.pickle","wb")
+#pickle.dump(classifier,save_classifier)
+#save_classifier.close()
 
 # Uses a wrapper around nltk classfier
 MNB_classifier = SklearnClassifier(MultinomialNB())
 MNB_classifier.train(training_set)
 print("Multinomial Classifier acc:", (nltk.classify.accuracy(MNB_classifier,test_set))*100)
 
+save_classifier = open("MNBClassifier.pickle","wb")
+pickle.dump(MNB_classifier,save_classifier)
+save_classifier.close()
+
 LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
 LogisticRegression_classifier.train(training_set)
 print("LogisticRegression Classifier acc:", (nltk.classify.accuracy(LogisticRegression_classifier,test_set))*100)
+
+save_classifier = open("logReg.pickle","wb")
+pickle.dump(LogisticRegression_classifier,save_classifier)
+save_classifier.close()
 
 SGD_classifier = SklearnClassifier(SGDClassifier())
 SGD_classifier.train(training_set)
 print("SGD Classifier acc:", (nltk.classify.accuracy(SGD_classifier,test_set))*100)
 
-SVC_classifier = SklearnClassifier(SVC())
-SVC_classifier.train(training_set)
-print("SVC Classifier acc:", (nltk.classify.accuracy(SVC_classifier,test_set))*100)
+#SVC_classifier = SklearnClassifier(SVC())
+#SVC_classifier.train(training_set)
+#print("SVC Classifier acc:", (nltk.classify.accuracy(SVC_classifier,test_set))*100)
+
+#save_classifier = open("SVC.pickle","wb")
+#pickle.dump(SVC_classifier,save_classifier)
+#save_classifier.close()
 
 LSVC_classifier = SklearnClassifier(LinearSVC())
 LSVC_classifier.train(training_set)
@@ -123,20 +139,19 @@ print("Nu SVC Classifier acc:", (nltk.classify.accuracy(NuSVC_classifier,test_se
 
 # Let's add a confidence parameter using a voting system. Next hack
 
-voted_classifier = VoteClassifier(classifier,
-				  MNB_classifier,
+voted_classifier = VoteClassifier(MNB_classifier,
 				  LogisticRegression_classifier, 
-				  SGD_classifier,
-			      	  SVC_classifier,
 				  LSVC_classifier,
 				  NuSVC_classifier)
 
 print("Voted classifier accuracy percent:",(nltk.classify.accuracy(voted_classifier,test_set))*100)
 
-print("Classification: ", voted_classifier.classify(test_set[0][0]), "Confidence: ", voted_classifier.confidence(test_set[0][0])*100)
+print("Classification: ", voted_classifier.classify(test_set[0][0]), "Confidence: ", voted_classifier.confidence(test_set[0][0]))
 
+print("Classification: ", voted_classifier.classify(test_set[1][0]), "Confidence: ", voted_classifier.confidence(test_set[1][0]))
 
-print("Classification: ", voted_classifier.classify(test_set[1][0]), "Confidence: ", voted_classifier.confidence(test_set[1][0])*100)
-print("Classification: ", voted_classifier.classify(test_set[2][0]), "Confidence: ", voted_classifier.confidence(test_set[2][0])*100)
-print("Classification: ", voted_classifier.classify(test_set[3][0]), "Confidence: ", voted_classifier.confidence(test_set[3][0])*100)
-print("Classification: ", voted_classifier.classify(test_set[4][0]), "Confidence: ", voted_classifier.confidence(test_set[4][0])*100)
+print("Classification: ", voted_classifier.classify(test_set[2][0]), "Confidence: ", voted_classifier.confidence(test_set[2][0]))
+
+print("Classification: ", voted_classifier.classify(test_set[4][0]), "Confidence: ", voted_classifier.confidence(test_set[3][0]))
+
+print("Classification: ", voted_classifier.classify(test_set[4][0]), "Confidence: ", voted_classifier.confidence(test_set[4][0]))
