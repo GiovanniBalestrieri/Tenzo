4e2997680a630d9e94e7eb33c14bfc61709b684a
userk <balestrieri.gepp@gmail.com>
2016-09-21 18:54:54 +0200

    Success: read lines and columns. I am at the hospital Grassi in Ostia btw

--- python/optimization/nltk/textClassification/openSets.py
+++ python/optimization/nltk/textClassification/openSets.py
@@ -7,24 +7,23 @@ test = csv.reader(io.open("test.csv","r",encoding='utf-8'),skipinitialspace=True
 
 
 # Training Set
-pos_training = io.open("/var/lib/mysql-files/TRAIN_POS_G_trust2012_4@hotmail.it.csv","r",encoding='utf-8').read()
-neg_training = io.open("/var/lib/mysql-files/TRAIN_POS_B_trust2012_4@hotmail.it.csv","r",encoding='utf-8').read()
-
+pos_training = io.open("/var/lib/mysql-files/TRAIN_POS_G_trust2012_4@hotmail.it.csv","r",encoding='utf-8')
+neg_training = io.open("/var/lib/mysql-files/TRAIN_POS_B_trust2012_4@hotmail.it.csv","r",encoding='utf-8')
 
 # Test Set
-pos_test = io.open("/var/lib/mysql-files/TEST_POS_trust2012@hotmail.it_all.csv","r",encoding='utf-8').read()
-neg_test = io.open("/var/lib/mysql-files/TEST_NEG_trust2012@hotmail.it_all.csv","r",encoding='utf-8').read()
+pos_test = io.open("/var/lib/mysql-files/TEST_POS_trust2012@hotmail.it_all.csv","r",encoding='utf-8')
+neg_test = io.open("/var/lib/mysql-files/TEST_NEG_trust2012@hotmail.it_all.csv","r",encoding='utf-8')
 
 
 ## Append tag to sets
-posTr = csv.reader(pos_training,delimiter=",",lineterminator="\n",dialect='excel',skipinitialspace=True)
+posTr = csv.reader(pos_training,skipinitialspace=True)
 data = list(posTr)
 test = list(test)
-try:
-	for w in test:
-		content = w
-	        print content
-		#print "obj num: ", len(w), "\tnew line"
-except csv.Error, e:
-	sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+for w in test:
+	print "New Row: "
+	print w[0]
+	#for h in w:
+	       #print h
+		
+	#print "obj num: ", len(w), "\tnew line"
 print len(test)
