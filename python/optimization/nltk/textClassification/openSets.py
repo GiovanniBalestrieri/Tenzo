c4f72010d70648d02dd6118fef9745f193894cdd
userk <balestrieri.gepp@gmail.com>
2016-09-21 17:58:28 +0200

    Trying to extract rows and columns from badly formatted csv file

--- python/optimization/nltk/textClassification/openSets.py
+++ python/optimization/nltk/textClassification/openSets.py
@@ -1,4 +1,9 @@
-import io
+import io,csv
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+test = csv.reader(io.open("test.csv","r",encoding='utf-8'),skipinitialspace=True)
 
 
 # Training Set
@@ -9,3 +14,17 @@ neg_training = io.open("/var/lib/mysql-files/TRAIN_POS_B_trust2012_4@hotmail.it.
 # Test Set
 pos_test = io.open("/var/lib/mysql-files/TEST_POS_trust2012@hotmail.it_all.csv","r",encoding='utf-8').read()
 neg_test = io.open("/var/lib/mysql-files/TEST_NEG_trust2012@hotmail.it_all.csv","r",encoding='utf-8').read()
+
+
+## Append tag to sets
+posTr = csv.reader(pos_training,delimiter=",",lineterminator="\n",dialect='excel',skipinitialspace=True)
+data = list(posTr)
+test = list(test)
+try:
+	for w in test:
+		content = w
+	        print content
+		#print "obj num: ", len(w), "\tnew line"
+except csv.Error, e:
+	sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+print len(test)
