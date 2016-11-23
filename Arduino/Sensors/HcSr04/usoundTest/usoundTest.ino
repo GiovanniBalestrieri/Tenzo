ac1846e92b1518fc6ae40c6acfdc25ea8d827596
userk <balestrieri.gepp@gmail.com>
2016-11-23 22:36:12 +0100

    Modified ultrasound

--- Arduino/Sensors/HcSr04/usoundTest/usoundTest.ino
+++ Arduino/Sensors/HcSr04/usoundTest/usoundTest.ino
@@ -6,14 +6,13 @@ long duration, distance;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(115200); 
-pinMode( trigPin, OUTPUT );
-pinMode( echoPin, INPUT );
+  Serial.begin(115200); 
+  pinMode( trigPin, OUTPUT );
+  pinMode( echoPin, INPUT );
+  Serial.println("OK");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+void loop() {  
   digitalWrite(trigPin, LOW);  // Added this line
   delayMicroseconds(20); // Added this line
   digitalWrite(trigPin, HIGH);
@@ -22,17 +21,16 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   long r = 0.034 * duration / 2;
   Serial.print( "durata: " );
-Serial.print( duration );
-Serial.print( " , " );
-Serial.print( "distanza: " );
- 
-//dopo 38ms è fuori dalla portata del sensore
-if( duration > 38000 ) 
-  Serial.println( "fuori portata");
-else { Serial.print( r ); 
-  Serial.println( "cm" );}
- 
-//aspetta 1.5 secondi
-delay( 20 );
-
+  Serial.print( duration );
+  Serial.print( " , " );
+  Serial.print( "distanza: " );
+   
+  //dopo 38ms è fuori dalla portata del sensore
+  if( duration > 38000 ) 
+    Serial.println( "fuori portata");
+  else { Serial.print( r ); 
+    Serial.println( "cm" );}
+   
+  //aspetta 1.5 secondi
+  delay( 20 );
 }
