c46382385890c89fea83eb7f22a241bb1d2626a0
userk <balestrieri.gepp@gmail.com>
2016-12-26 03:10:35 +0100

    Added response handler flask

--- python/webbase/flask/base.py
+++ python/webbase/flask/base.py
@@ -1,10 +1,23 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, Response,json
 
 app = Flask(__name__)
 
 @app.route('/')
 def api_root():
     return 'Welcome'
+
+@app.route('/hello', methods = ['GET'])
+def api_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
 
 @app.route('/articles')
 def api_articles():
